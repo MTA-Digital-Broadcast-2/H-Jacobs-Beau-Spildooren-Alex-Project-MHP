e580795dad6ab832c3073c7d178cb495749c1eee
SpildoorenAlex <alex.spildooren@student.kdg.be>
2015-06-08 01:23:59 +0200

    java for alex
    Java oefeningen (commits lijken niet te werken)

---
+++ Spildooren-Alex/Labojava/blz5/Eersteprog.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+
+public class Eersteprog{
+	public static void main(String args[])
+		{
+		System.out.println("holy fuck dees werkt ook met ++?");
+		int a;
+		float b ;
+		a=5;
+		a=a+1;
+		b=3.3F;
+		b=b-0.1f;
+		System.out.println("a= "+a+"  ||||  b="+b); 
+		boolean x = (a > b);
+		System.out.println("x= "+x); 
+if(a==b){System.out.println("a is gelijk aan b"); }
+		}
+}
+	
+	
